b290bdc1cd90424c77f51f9f159ccdd5fb4b8be0
msilvaalcalde <msilvaalcalde@gmail.com>
2026-05-02 20:29:44 -0500

    Creacion de atributos de la clse Paciente

--- src/caso/hospital/xyz/Paciente.java
+++ src/caso/hospital/xyz/Paciente.java
@@ -4,10 +4,22 @@
  */
 package caso.hospital.xyz;
 
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  *
  * @author marantonio55
  */
 public class Paciente {
+    private String nombresCompletos;
+    private String tipoDocumento;
+    private String nroIndentificacion;
+    private LocalDate fechaNacimiento;
+    private String tipoSangre;
+    private List<String> tipoAlergias;
+    
+    
+    
     
 }
